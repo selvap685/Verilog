d1748f6de2aacd15dd2b631af6671693244b0492
selvap685 <145105065+selvap685@users.noreply.github.com>
2025-06-25 09:56:52 +0530

    Update replicationoperator.v

--- Hdlbits/vectors/replicationoperator.v
+++ Hdlbits/vectors/replicationoperator.v
@@ -1,6 +1,5 @@
-module top_module( 
+module top_module (
     input [7:0] in,
-    output [7:0] out
-);
-  assign out={in[0],in[1],in[2],in[3],in[4],in[5],in[6],in[7]};
+    output [31:0] out );
+    assign out = { {24{in[7]}}, in};
 endmodule
