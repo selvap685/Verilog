331f172221e088bf51141f5adb0f76e1f5153396
selvap685 <145105065+selvap685@users.noreply.github.com>
2025-06-26 16:39:55 +0530

    Create conditional.v

---
+++ Hdlbits/More Verilog Features/conditional.v
@@ -0,0 +1,8 @@
+module top_module (
+    input [7:0] a, b, c, d,
+    output [7:0] min);//
+    wire[7:0] min1 ,min2;
+    assign min1 = (a>b)? b: a;
+    assign min2 = (c>d)? d:c;
+    assign min=(min1>min2)?min2:min1;
+endmodule
